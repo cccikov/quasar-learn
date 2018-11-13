6526d2da369ae81b91e626f1a41beab0fc5201ff
ccc <522978139@qq.com>
2018-11-13 16:57:16 +0800

    侧栏

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -11,12 +11,20 @@
 					Quasar App
 					<div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
 				</q-toolbar-title>
+
+				<q-btn flat dense round @click="$router.push('/')" aria-label="首页">
+					<q-icon name="home" />
+				</q-btn>
 			</q-toolbar>
 		</q-layout-header>
 
 		<q-layout-drawer v-model="leftDrawerOpen" :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null">
 			<q-list no-border link inset-delimiter>
 				<q-list-header>Essential Links</q-list-header>
+				<q-item @click.native="$router.push('/')">
+					<q-item-side icon="home" />
+					<q-item-main label="首页" sublabel="我这是首页" />
+				</q-item>
 				<q-item @click.native="openURL('http://quasar-framework.org')">
 					<q-item-side icon="school" />
 					<q-item-main label="Docs" sublabel="quasar-framework.org" />
