f3bfbe75e3427636e6f54bc9bf0d37a363b22e9f
ccc <522978139@qq.com>
2018-11-17 19:00:00 +0800

    scroll

--- src/layouts/scroll.vue
+++ src/layouts/scroll.vue
@@ -1,10 +1,10 @@
 <template>
     <q-layout>
         <q-page-container>
-            <q-infinite-scroll :handler="loadMore">
+            <q-infinite-scroll ref="infiniteScroll" :handler="loadMore">
                 <!-- 内容, 在本例中是一些<p>标签 -->
                 <p v-for="item in items" class="caption">
-                    Lorem ipsum dolor sit amet...
+                    Lorem ipsum dolor sit amet({{item}})...
                 </p>
                 <q-spinner-dots slot="message" :size="40"></q-spinner-dots>
             </q-infinite-scroll>
@@ -17,14 +17,36 @@
     export default {
         data() {
             return {
-                items:[1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8]
+                items: []
             };
         },
         methods: {
-            loadMore(index, done) {}
+            addData() {
+                let list = [];
+                for (let i = 0; i < 3; i++) {
+                    list.push(Math.random());
+                }
+                return list;
+            },
+            loadMore(index, done) {
+                // this.$refs.infiniteScroll.stop
+                console.log(index);
+                // console.log(done);
+                // if (index > 100) {
+                //     done(true);
+                // }
+                setTimeout(() => {
+                    let list = this.addData();
+                    this.items = this.items.concat(list);
+                    done(); // 不传参数相当于 poll() 检查滚动位置并根据需要加载更多内容。传入true相当于 stop() 无论滚动位置在哪，停止工作。
+                });
+            }
         }
     };
 </script>
 
 <style>
+    .caption {
+        margin: 50px 0;
+    }
 </style>
