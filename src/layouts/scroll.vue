724f2e3177c63e8ff3b0ed6fd24780aec53069bf
ccc <522978139@qq.com>
2018-11-19 17:13:19 +0800

    scroll

--- src/layouts/scroll.vue
+++ src/layouts/scroll.vue
@@ -1,12 +1,16 @@
 <template>
     <q-layout>
         <q-page-container>
-            <q-infinite-scroll ref="infiniteScroll" :handler="loadMore">
+            <q-btn label="切换" @click="change" />
+            <q-infinite-scroll ref="xiaoyu" :handler="resfresher">
                 <!-- 内容, 在本例中是一些<p>标签 -->
                 <p v-for="item in items" class="caption">
                     Lorem ipsum dolor sit amet({{item}})...
                 </p>
-                <q-spinner-dots slot="message" :size="40"></q-spinner-dots>
+
+                <div class="text-center" slot="message">
+                    <q-spinner-dots :size="40"></q-spinner-dots>
+                </div>
             </q-infinite-scroll>
 
         </q-page-container>
@@ -14,31 +18,46 @@
 </template>
 
 <script>
+    function axios() {
+        return new Promise(function(resolve, reject) {
+            setTimeout(() => {
+                let list = [];
+                for (let i = 0; i < 30; i++) {
+                    list.push(Math.random());
+                }
+                resolve(list);
+            }, 300);
+        });
+    }
+
     export default {
+        create() {
+            console.log("a页面onresize");
+            debugger;
+            window.onresize = function() {};
+        },
         data() {
             return {
                 items: []
             };
         },
         methods: {
-            addData() {
-                let list = [];
-                for (let i = 0; i < 3; i++) {
-                    list.push(Math.random());
-                }
-                return list;
+            change() {
+                this.items = [];
+                this.$refs.xiaoyu.reset(); // 将索引重置为0.
+                this.$refs.xiaoyu.resume(); // 开始（恢复）工作
+                this.$refs.xiaoyu.poll(); // 检查滚动位置并根据需要加载更多内容。
             },
-            loadMore(index, done) {
-                // this.$refs.infiniteScroll.stop
+            resfresher(index, done) {
                 console.log(index);
-                // console.log(done);
-                // if (index > 100) {
-                //     done(true);
-                // }
-                setTimeout(() => {
-                    let list = this.addData();
+
+                axios().then(list => {
                     this.items = this.items.concat(list);
-                    done(); // 不传参数相当于 poll() 检查滚动位置并根据需要加载更多内容。传入true相当于 stop() 无论滚动位置在哪，停止工作。
+                    if (index == 3) {
+                        done(true); // 全部数据加载好了，停止工作
+                    } else {
+                        done(); // 不传参数相当于 poll() 检查滚动位置并根据需要加载更多内容。传入true相当于 stop() 无论滚动位置在哪，停止工作。
+                    }
                 });
             }
         }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,24 +1,36 @@
-
-const routes = [
-	{
-		path: '/',
-		component: () => import('layouts/MyLayout.vue'),
-		children: [
-            { path: '', component: () => import('pages/Index.vue') },
-			{ path: '/tabs', component: () => import('pages/tabs.vue') },
-			{ path: '/console', component: () => import('pages/console.vue') },
-			{ path: '/web', component: () => import('pages/web.vue') },
-			{ path: '/scroll', component: () => import('layouts/scroll.vue') }
-		]
-	}
+const routes = [{
+        path: '/',
+        component: () => import('layouts/MyLayout.vue'),
+        children: [{
+                path: '',
+                component: () => import('pages/Index.vue')
+            },
+            {
+                path: '/tabs',
+                component: () => import('pages/tabs.vue')
+            },
+            {
+                path: '/console',
+                component: () => import('pages/console.vue')
+            },
+            {
+                path: '/web',
+                component: () => import('pages/web.vue')
+            },
+        ]
+    },
+    {
+        path: '/scroll',
+        component: () => import('layouts/scroll.vue')
+    }
 ]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-	routes.push({
-		path: '*',
-		component: () => import('pages/Error404.vue')
-	})
+    routes.push({
+        path: '*',
+        component: () => import('pages/Error404.vue')
+    })
 }
 
 export default routes
